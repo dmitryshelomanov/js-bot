79aadb2a7c5b9fbe5846f2874f030bb763daa818
Shelomanov Dmitry <userbq201@mail.ru>
2018-01-27 14:26:48 +0300

    update gitognore

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const debug = require('debug')('js-bot:index')
+const Telegraf = require('telegraf')
+const { handlerCode } = require('./src/handlers/code')
 
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load()
 }
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot
+  .hears(new RegExp(/!code\s([^.+$]+)/), handlerCode)
+  .startPolling()
