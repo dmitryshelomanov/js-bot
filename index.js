5e0af68c1c72d36f2d74034209d5cf2f0f4943c0
Shelomanov Dmitry <userbq201@mail.ru>
2018-01-27 23:21:40 +0300

    update

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const debug = require('debug')('js-bot:index')
 const Telegraf = require('telegraf')
 const { handlerCode } = require('./src/handlers/code')
 
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load()
-}
+require('dotenv').load()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
