942c8395754c5ffdf49591f8d37ff21ee64dfa9f
Shelomanov Dmitry <userbq201@mail.ru>
2018-01-27 18:37:54 +0300

    add help notice

--- index.js
+++ index.js
@@ -12,5 +12,18 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 debug('bot started')
 
 bot
-  .hears(new RegExp(/!code/), handlerCode)
+  .hears(new RegExp(/!code/), handlerCode)  
+  .command('help', ({ replyWithMarkdown }) => {
+    replyWithMarkdown(`
+      *js sandbox bot*
+      responds to a message of the form
+      \`!code you_js_code\` \n
+      Context have
+      _setTimeout_
+      _Promise_
+      _console_ \n
+      *Notice*
+      \`if you're request have a long (> 10s) bot throws an exception\`
+    `)
+  })
   .startPolling()
