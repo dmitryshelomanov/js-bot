65222df6f996485c47676688318291a1ee331cd2
Shelomanov Dmitry <userbq201@mail.ru>
2018-01-27 14:26:22 +0300

    refactore sandbox

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,16 +1,20 @@
+const debug = require('debug')('js-bot:sandbox')
 const { resolve } = require('path')
 const fs = require('fs-extra')
+const uid = require('./utils/uid')
 const { asyncExec } = require('./utils/exec-promisify')
 const { boilerplateGenerated } = require('./utils/boilerplate-code')
 
 
-async function createSandbox(code) {
+async function createSandbox(strCode) {
   try {
-    const path = resolve(__dirname, '..' ,'tmp', 'index.js')
+    const fileName = `${uid()}.js`
+    const path = resolve(__dirname, '..', 'tmp', fileName)
     const code = boilerplateGenerated(strCode)
 
+    debug('path - ', path)
     await fs.writeFile(path, code)
-    const rs = await asyncExec('node tmp/index.js')
+    const rs = await asyncExec(`node tmp/${fileName}`)
 
     await fs.unlink(path)
     return rs
