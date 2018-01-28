b74c2e2eca2dd14985a349dc4fc852286665aa10
Shelomanov Dmitry <userbq201@mail.ru>
2018-01-28 04:50:06 +0300

    add escape

--- src/utils/boilerplate-code.js
+++ src/utils/boilerplate-code.js
@@ -1,16 +1,15 @@
 const debug = require('debug')('js-bot:boilerplate-code')
+const et = require('./escape-tilde')
 
 
 function boilerplateGenerated(code) {
   debug('template formated')
   return `
     const vm = require('vm')
-    const sandbox = {
-      console,
-      setTimeout,
-    }
+    const sandbox = require('../src/utils/context')
+
     vm.createContext(sandbox)
-    vm.runInContext(\`${code}\`, sandbox)
+    vm.runInContext(\`${et(code)}\`, sandbox)
   `
 }
 
